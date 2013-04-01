0350e3f235baae195c6426cd57ec074377677905
chzhour <chzhour@mail2.sysu.edu.cn>
2013-04-01 19:18:10 +0800

    fix bug

--- src/com/googlecode/gycreative/MainActivity.java
+++ src/com/googlecode/gycreative/MainActivity.java
@@ -9,19 +9,22 @@ import com.googlecode.gycreative.fastlist.Item;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private FastListView listView;
-	ArrayList<Item>list;
+	ArrayList<Item> list;
 	FastListAdapter adapter;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		ConfigScreen.init(MainActivity.this);
-		listView = (FastListView)findViewById(R.id.listView);
+		buildFileDir();
+		
+		listView = (FastListView) findViewById(R.id.listView);
 		list = TestData.initalizeData(MainActivity.this);
 		adapter = new FastListAdapter(list);
 		listView.setAdapter(adapter);
@@ -38,6 +41,26 @@ public class MainActivity extends Activity {
 			return;
 		for (File f : files)
 			f.delete();
-		
+	}
+
+	private void buildFileDir() {
+		if (hasSdcard()) {
+			File file = new File(Environment.getExternalStorageDirectory()+"/listCache");
+			if (!file.exists()) {
+				file.mkdir();
+			}
+		}
+		else{
+			Toast.makeText(MainActivity.this, "«Î≤Â»ÎSDø®", Toast.LENGTH_LONG).show();
+		}
+	}
+
+	public static boolean hasSdcard() {
+		String status = Environment.getExternalStorageState();
+		if (status.equals(Environment.MEDIA_MOUNTED)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
